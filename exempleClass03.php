f1cb0ea7bb479a2b1678c7e46d76cc0f1beee3bb
mikhawa <michaeljpitz@gmail.com>
2019-05-13 09:51:31 +0200

    exemple class 03

---
+++ exempleClass03.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Un fichier par class, le nom du fichier est le même que le nom de la classe ( pour s'y retrouver mais surtout pour générer un autoload de classe)
+ */
+class exempleClass03
+{
+
+}
